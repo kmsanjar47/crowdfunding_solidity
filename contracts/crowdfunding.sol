e3eee4efda920eeef947eef6ca085743414de0d0
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2023-11-11 00:07:43 +0600

    initial

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -12,8 +12,9 @@ contract CrowdFunding {
         uint256 deadline;
         mapping(address => uint256) donators;
     }
-
-    Campaign[] public campaigns;
+    mapping (uint => Campaign) public campaigns;
+    uint campaignNo = 0;
+    
 
     // modifier ownerOnlyArea(){
     //     require(msg.sender == owner,"Only the owner of the campaign can access this");
@@ -34,16 +35,26 @@ contract CrowdFunding {
         uint256 _target,
         uint256 _campaignDuration
     ) public {
-        Campaign memory newCampaign = Campaign({
-            owner: msg.sender,
-            campaignName: _campaignName,
-            donationReceiver: _donatonReceiver,
-            target: _target,
-            isOpen: true,
-            totalBalance: 0,
-            deadline: _campaignDuration
-        });
+        
+        // Campaign memory newCampaign = Campaign({
+        //     owner: msg.sender,
+        //     campaignName: _campaignName,
+        //     donationReceiver: _donatonReceiver,
+        //     target: _target,
+        //     isOpen: true,
+        //     totalBalance: 0,
+        //     deadline: _campaignDuration
+        // });
+        Campaign storage newCampaign = campaigns[campaignNo];
+        newCampaign.owner = msg.sender;
+        newCampaign.campaignName= _campaignName;
+        newCampaign.donationReceiver= _donatonReceiver;
+        newCampaign.target= _target;
+        newCampaign.isOpen= true;
+        newCampaign.totalBalance= 0;
+        newCampaign.deadline= _campaignDuration;
 
-        campaigns.push(newCampaign);
+        campaignNo ++;
+
     }
 }
