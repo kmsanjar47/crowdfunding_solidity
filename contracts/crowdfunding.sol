fd4237f08f6e371d7d6e7a3642c98308cf4e10ec
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2023-11-12 01:55:38 +0600

    improvements

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -5,15 +5,126 @@ contract CrowdFunding {
     struct Campaign {
         address owner;
         string campaignName;
-        address payable donationReceiver;
+        address donationReceiver;
         uint256 target;
         bool isOpen;
         uint256 totalBalance;
         uint256 deadline;
+        bool isFailed;
+        address[] donatorsAddress;
         mapping(address => uint256) donators;
     }
+
     mapping(uint256 => Campaign) public campaigns;
     uint256 campaignNo = 0;
+
+    //Events
+
+    event targetReached(
+        string _campaignName,
+        uint256 _targetAmount,
+        uint256 _currentBalance
+    );
+    event fundSentToReceiver(address _receiverAddress, uint256 _receivedAmount);
+    event campaignDeadlineReached(string _campaignName);
+    event campaignFailedToReachTarget(string _campaignName);
+    event donatorsRefunded(address[] _donatorAddress);
+    event donationReceived(address _donatorAddress, uint256 _amount);
+
+    //Modifiers
+
+    modifier campaignIsActive(uint256 _campaignNo) {
+        require(
+            campaigns[_campaignNo].isOpen,
+            "Campaign Not Valid or Open Anymore"
+        );
+        _;
+    }
+    modifier campaignExists(string memory _campaignName) {
+        require(
+            fetchCampaignIndex(_campaignName) != type(uint256).max,
+            "Campaign doesn't exist. Please re-check..."
+        );
+        _;
+    }
+    modifier deadlineReached(uint256 _campaignNo) {
+        require(
+            campaigns[_campaignNo].deadline < block.timestamp,
+            "Campaign reached the deadline. You can't do any transaction anymore"
+        );
+        _;
+    }
+
+    modifier campaignFailed(uint256 _campaignNo) {
+        require(
+            campaigns[_campaignNo].isFailed,
+            "Campaign currenlty open or successfully reached the target amount within deadline"
+        );
+        _;
+    }
+
+    modifier checkDonatorValidity(uint256 _campaignNo) {
+        require(
+            campaigns[_campaignNo].donators[msg.sender] != 0,
+            "You can't access this. Possible reasons: You are not a donator in this campaign or your donated fund is already withdrawn"
+        );
+        _;
+    }
+    modifier userRestrictedArea(uint256 _campaignNo) {
+        require(
+            msg.sender != campaigns[_campaignNo].owner,
+            "Owners can't access this area"
+        );
+        _;
+    }
+
+    //Functions
+
+    //Helper Functions
+
+    function fetchCampaignIndex(string memory _campaignName)
+        private
+        view
+        returns (uint256 _index)
+    {
+        for (uint256 i = 0; i < campaignNo; i++) {
+            if (
+                keccak256(abi.encodePacked(campaigns[i].campaignName)) ==
+                keccak256(abi.encodePacked(_campaignName))
+            ) {
+                return i;
+            }
+        }
+        return type(uint256).max;
+    }
+
+    function isTargetReached(uint256 _campaignNo)
+        private
+        view
+        returns (bool _isReached)
+    {
+        if (
+            campaigns[_campaignNo].totalBalance >= campaigns[_campaignNo].target
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    function isDeadlineReached(uint256 _campaignNo)
+        private
+        view
+        returns (bool _isReached)
+    {
+        if (campaigns[_campaignNo].deadline < block.timestamp) {
+            return true;
+        }
+        return false;
+    }
+
+    //Main Functions
+
+    //Create a new campaign
 
     function createCampaign(
         string memory _campaignName,
@@ -28,114 +139,66 @@ contract CrowdFunding {
         newCampaign.target = _target;
         newCampaign.isOpen = true;
         newCampaign.totalBalance = 0;
-        newCampaign.deadline = _campaignDuration + (block.timestamp * 1 days);
+        newCampaign.isFailed = false;
+        newCampaign.deadline = block.timestamp + (_campaignDuration * 1 days);
 
         campaignNo++;
     }
 
-    event targetReached(
-        string _campaignName,
-        uint256 _targetAmount,
-        uint256 _currentBalance
-    );
-    event fundSentToReceiver(address _receiverAddress, uint256 _receivedAmount);
-    event campaignDeadlineReached(string _campaignName);
-    event donatorsRefunded(address[] _donatorAddress);
-    event donationReceived(address _donatorAddress, uint256 _amount);
-
-    function campaignNameMatches(string memory _campaignName)
-        private
-        view
-        returns (bool _isMatched)
+    function donateToCampaign(string memory _campaignName)
+        public
+        payable
+        deadlineReached(fetchCampaignIndex(_campaignName))
+        campaignIsActive(fetchCampaignIndex(_campaignName))
+        campaignExists(_campaignName)
     {
-        for (uint256 i = 0; i < campaignNo; i++) {
-            if (
-                keccak256(abi.encodePacked(campaigns[i].campaignName)) ==
-                keccak256(abi.encodePacked(_campaignName))
-            ) {
-                return true;
-            }
-        }
-        return false;
-    }
+        uint256 i = fetchCampaignIndex(_campaignName);
 
-    function campaignIsActive(string memory _campaignName)
-        private
-        view
-        returns (bool _campaignStatus)
-    {
-        for (uint256 i = 0; i < campaignNo; i++) {
-            if (
-                keccak256(abi.encodePacked(campaigns[i].campaignName)) ==
-                keccak256(abi.encodePacked(_campaignName))
-            ) {
-                if (campaigns[i].isOpen) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+        campaigns[i].donators[msg.sender] = msg.value;
+        campaigns[i].donatorsAddress.push(msg.sender);
+        campaigns[i].totalBalance += msg.value;
 
-    function isTargetReached(uint256 _campaignNo)
-        private view
-        returns (bool _isReached)
-    {
-        if (
-            campaigns[_campaignNo].totalBalance >= campaigns[_campaignNo].target
-        ) {
-            return true;
-        }
-        return false;
-    }
-
-    function isDeadlineReached(uint _campaignNo) private view returns(bool _isReached){
-        if(campaigns[_campaignNo].deadline > (block.timestamp * 1 days)){
-            return true;
-        }
-        return false;
+        emit donationReceived(msg.sender, msg.value);
+        checkCampaignState(i);
     }
 
     function checkCampaignState(uint256 _campaignNo) private {
         if (isTargetReached(_campaignNo)) {
             campaigns[_campaignNo].isOpen = false;
-             emit targetReached(
+            emit targetReached(
                 campaigns[_campaignNo].campaignName,
                 campaigns[_campaignNo].target,
                 campaigns[_campaignNo].totalBalance
             );
             releaseFundToReceiver(_campaignNo);
-        }
-        else if(isDeadlineReached(_campaignNo)){
+        } else if (isDeadlineReached(_campaignNo)) {
             campaigns[_campaignNo].isOpen = false;
+            campaigns[_campaignNo].isFailed = true;
             emit campaignDeadlineReached(campaigns[_campaignNo].campaignName);
-            releaseFundToDonators(_campaignNo);
+            emit campaignFailedToReachTarget(campaigns[_campaignNo].campaignName);
         }
-        
     }
 
-    function donateToCampaign(string memory _campaignName) public payable {
-        require(
-            campaignIsActive(_campaignName),
-            "Campaign Not Valid or Not Open Anymore"
+    function releaseFundToReceiver(uint256 _campaignNo) public payable {
+        address payable receiverAddress = payable(
+            campaigns[_campaignNo].donationReceiver
         );
-        for (uint256 i = 0; i < campaignNo; i++) {
-            if (campaignNameMatches(_campaignName))
-                campaigns[i].donators[msg.sender] = msg.value;
-            campaigns[i].totalBalance += msg.value;
-            emit donationReceived(msg.sender, msg.value);
-            checkCampaignState(i);
-        }
-    }
-
-    function releaseFundToReceiver(uint _campaignNo) public payable {
-        address payable receiverAddress = campaigns[_campaignNo].donationReceiver;
-        uint totalBalance = campaigns[_campaignNo].totalBalance;
+        uint256 totalBalance = campaigns[_campaignNo].totalBalance;
         receiverAddress.transfer(totalBalance);
         emit fundSentToReceiver(receiverAddress, totalBalance);
     }
 
-    function releaseFundToDonators(uint _campaignNo) public payable {
 
+    function withdrawHeldFund(uint256 _campaignNo)
+        public
+        payable
+        userRestrictedArea(_campaignNo)
+        campaignFailed(_campaignNo)
+        checkDonatorValidity(_campaignNo)
+    {
+
+        address payable donatorAddress = payable(msg.sender);
+        uint256 donatedAmount = campaigns[_campaignNo].donators[msg.sender];
+        donatorAddress.transfer(donatedAmount);
     }
 }
